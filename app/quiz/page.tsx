dc6eea0e56a1e3d03eba7c2e7536f2a01e31126d
mac <mac@MacBook-Pro.local>
2025-12-16 16:54:19 +0800

    Fix VotingCard import path in quiz page

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import VotingCard from './components/VotingCard';
+import VotingCard from '../components/VotingCard';
 
 interface ContentPair {
   id: string;
